601a531af1950e9d3c44477c3a4133d9f8efe5df
samuelbaldasso <samuelbaldasso93@gmail.com>
2023-06-19 14:48:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+// Questão 1
+
+console.log("Hello World!");
+
+// Questão 2
+
+const n1 = 8;
+const n2 = 10;
+const sum = n1 + n2;
+
+console.log(sum);
+
+// Questão 3
+
+const numero = 4;
+
+if (typeof numero === "number") {
+  console.log("Número.");
+} else {
+  console.log("Não é um número.");
+}
+
+// Questão 4
+
+const string = "4";
+
+if (typeof string === "string") {
+  console.log("String.");
+} else {
+  console.log("Não é uma string.");
+}
+
+// Questão 5
+
+const boolean = true;
+
+if (typeof boolean === "boolean") {
+  console.log("Booleano.");
+} else {
+  console.log("Não é um booleano.");
+}
+
+// Questão 6
+
+const n3 = 18;
+const n4 = 10;
+const subtract = n3 - n4;
+
+console.log(subtract);
+
+// Questão 7
+
+const n5 = 18;
+const n6 = 10;
+const multiply = n5 * n6;
+
+console.log(multiply);
+
+// Questão 8
+
+const n7 = 8;
+const n8 = 10;
+const division = n7 / n8;
+
+console.log(division);
+
+//  Questão 9
+
+const numero2 = 40;
+const isEven = numero2 % 2 === 0;
+
+if(isEven){
+  console.log("É um número par.")
+}else{
+  console.log("Não é um número par.")
+}
+
+//  Questão 10
+
+const numero3 = 435;
+const isOdd = numero2 % 2 !== 0;
+
+if(isOdd){
+  console.log("É um número ímpar.")
+}else{
+  console.log("Não é um número ímpar.")
+}
